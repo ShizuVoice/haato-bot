0aa7752119d63c66abbe531acdcbfeb157547d0b
SilentVOEZ <65421364+SilentVOEZ@users.noreply.github.com>
2021-03-07 22:02:20 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,40 +4,90 @@ import time
 import os
 import sys
 import psutil
+import json
 
 import asyncio
 from discord.ext import commands
 
 start_time = time.time()
+consoletime = datetime.datetime.now()
 
 TOKEN = open("./token.txt","r").read()
-PREFIX = open("./prefix.txt","r").read()
+MAINPREFIX = open("./prefix.txt","r").read()
 
 ID = open ("./author.txt", "r").read()
 ID_list = ID.split()
 map_object = map(int, ID_list)
 list_IDs = list(map_object)
 
-bot = commands.Bot(command_prefix=PREFIX, owner_ids=list_IDs)
+def get_prefix(bot, message):
+    with open("./serverprefix.json", "r") as f:
+        prefixes = json.load(f)
+
+    return prefixes[str(message.guild.id)]
+
+bot = commands.Bot(command_prefix= get_prefix, owner_ids=list_IDs)
 bot.remove_command('help')
 
 @bot.event
 async def on_ready():
-    prefix = open('./prefix.txt','r').read()
+    #prefix = open('./prefix.txt','r').read()
     print(f'--------------------------------')
     print(f'Logged in as: {bot.user.name}')
     print(f'With ID: {bot.user.id}')
     print(f'--------------------------------')
     print(f'Ensure that the bot has adequate permission to prevent errors while in use.')
     print(f'--------------------------------')
-    activity = discord.Game(name=f"{prefix}help | Beta Version", type=3)
+    activity = discord.Game(name=f"Ping me for the prefix | Beta Version", type=3)
     await bot.change_presence(status=discord.Status.dnd, activity=activity)
+
+@bot.event
+async def on_guild_join(guild):
+    with open("./serverprefix.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes[str(guild.id)] = MAINPREFIX
+
+    with open("./serverprefix.json", "w") as f:
+        json.dump(prefixes, f)
+
+    print(f"--------------------------------")
+    print(f"{consoletime} [INFO] {bot.user.name} joined to {guild.name} with ID {guild.id}")
+    print(f"--------------------------------")
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def changeprefix(ctx, prefix):
+    with open("./serverprefix.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes[str(ctx.guild.id)] = prefix
+
+    with open("./serverprefix.json", "w") as f:
+        json.dump(prefixes, f)
+
+    await ctx.send(f"The prefix was change to `{prefix}`")
+
+@bot.event
+async def on_message(msg):
+    try:
+        if msg.mentions[0]  == bot.user:
+            with open("./serverprefix.json", "r") as f:
+                prefixes = json.load(f)
+
+            pre = prefixes[str(msg.guild.id)]
+
+            await msg.channel.send(f"My prefix for this server is `{pre}` \nTo check the commands, type `{pre}help`")
+    except:
+        pass
+
+    await bot.process_commands(msg)
 
 # Extension commands
 @bot.command()
 @commands.is_owner()
 async def load(ctx, extension):
-    consoletime = datetime.datetime.now()
+    #consoletime = datetime.datetime.now()
     try:
         bot.load_extension(f'plugins.{extension}')
         embed = discord.Embed(colour = 0xdfe30b)
@@ -63,7 +113,7 @@ async def load(ctx, extension):
 @bot.command()
 @commands.is_owner()
 async def unload(ctx, extension):
-    consoletime = datetime.datetime.now()
+    #consoletime = datetime.datetime.now()
     try:
         bot.unload_extension(f'plugins.{extension}')
         embed = discord.Embed(colour = 0xdfe30b)
@@ -89,7 +139,7 @@ async def unload(ctx, extension):
 @bot.command()
 @commands.is_owner()
 async def reload(ctx, extension):
-    consoletime = datetime.datetime.now()
+    #consoletime = datetime.datetime.now()
     try:
         bot.unload_extension(f'plugins.{extension}')
         bot.load_extension(f'plugins.{extension}')
@@ -116,24 +166,25 @@ async def reload(ctx, extension):
 @bot.command()
 @commands.is_owner()
 async def loadall(ctx):
-    consoletime = datetime.datetime.now()
+    #consoletime = datetime.datetime.now()
     for filename in os.listdir('./plugins'):
         consoletime = datetime.datetime.now()
         if filename.endswith('.py'):
             try:
                 bot.load_extension(f'plugins.{filename[:-3]}')
                 await asyncio.sleep(3)
-                await ctx.send(f"**{filename}** extensions loaded.")
+                await ctx.send(f"**{filename}** loaded.")
                 print(f'{consoletime} [INFO] {filename} loaded')
             except:
-                print(f"{consoletime} [WARNING] Failed to reload {filename}. Please check the extension's code.")
+                await ctx.send(f"**{filename}** extension already loaded.")
+                print(f"{consoletime} [WARNING] Failed to load {filename}. Please check the extension's code.")
                 pass
 
 @bot.command()
 @commands.is_owner()
 async def unloadall(ctx):
     for filename in os.listdir('./plugins'):
-        consoletime = datetime.datetime.now()
+        #consoletime = datetime.datetime.now()
         if filename.endswith('.py'):
             try:
                 bot.unload_extension(f'plugins.{filename[:-3]}')
@@ -141,13 +192,14 @@ async def unloadall(ctx):
                 await ctx.send(f"**{filename}** unloaded")
                 print(f'{consoletime} [INFO] {filename} extension unloaded.')
             except:
+                await ctx.send(f"**{filename}** extension already unloaded.")
                 print(f"{consoletime} [WARNING] Failed to unload {filename}. Extension is already unloaded.")
                 pass
 
 @bot.command()
 @commands.is_owner()
 async def shutdown(ctx, *, reason = None):
-    consoletime = datetime.datetime.now()
+    #consoletime = datetime.datetime.now()
     embed = discord.Embed(colour = discord.Colour.red())
     
     current_time = time.time()
@@ -176,5 +228,7 @@ for filename in os.listdir('./plugins'):
     if filename.endswith('.py'):
         bot.load_extension(f'plugins.{filename[:-3]}')
         print(f'{consoletime} [INFO] {filename} loaded')
+
+print(f'\33]0;"Haato Bot - Dev Build"\a', end='', flush=True)
 
 bot.run(TOKEN)
